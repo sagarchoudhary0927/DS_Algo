443a7d60994f778d9771906c32e41f61b20b3fb3
Sagar Choudhary <sagarchaudhury@playsimple.in>
2023-03-10 00:02:37 +0530

    initialising array with different styles

--- Basic/Array/firstArray.cpp
+++ Basic/Array/firstArray.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main()
 {
-    int marks[10];
+    int marks[10]; // All cell of array has garbarge value as it is not initilaised
+    int arr1[10] = {0}; // Initialise first cell with 0 and all other has by default 0 value.
+    int arr2[10] = {1, 2, 3}; // First three index has values 1,2,3 all other has deafult 0. 
+
     int n;
     cout << "Enter the Number of students";
     cin >> n;
