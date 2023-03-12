4aedd51a0149559175c71da1aab6119203aa0835
Sagar Choudhary <sagarchaudhury@playsimple.in>
2023-03-12 13:31:07 +0530

    Pushing subarray sum uysing brute force

--- Basic/Array/subArray.cpp
+++ Basic/Array/subArray.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 void printAllSubArray(int *arr, int size)
 {
-    for (int i = 0; i < size; i++) {
-        for (int j = i; j < size; j++) {
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i; j < size; j++)
+        {
             cout << "(";
             for (int k = i; k <= j; k++)
             {
@@ -15,13 +17,33 @@ void printAllSubArray(int *arr, int size)
     }
 }
 
+// Printing subarray sum per line using brute force. Time complexity will be O(n^3) 
+void printSubArraySumBruteForce(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i; j < size; j++)
+        {
+            int sum = 0;
+            for (int k = i; k <= j; k++)
+            {
+                sum += arr[k];
+            }
+            cout << sum << ", ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int arr[] = {10, 20, 30, 40, 50, 60};
     int size = sizeof(arr) / sizeof(int);
 
-    printAllSubArray(arr, size); // Time complexity is O(n^3)
+    //printAllSubArray(arr, size); // Time complexity is O(n^3)
     // There are toal of n^2 subarray of n size array
+
+    printSubArraySumBruteForce(arr, size);
 
     return 0;
 }
