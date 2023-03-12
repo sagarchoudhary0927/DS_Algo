01bbb3356ae92e65210b62184dfdce4d8a810f59
Sagar Choudhary <sagarchaudhury@playsimple.in>
2023-03-12 13:41:30 +0530

    Pushing biggest subarray sum and biggest array with teh biggest subarray sum

--- Basic/Array/subArray.cpp
+++ Basic/Array/subArray.cpp
@@ -17,7 +17,7 @@ void printAllSubArray(int *arr, int size)
     }
 }
 
-// Printing subarray sum per line using brute force. Time complexity will be O(n^3) 
+// Printing subarray sum per line using brute force. Time complexity will be O(n^3)
 void printSubArraySumBruteForce(int *arr, int size)
 {
     for (int i = 0; i < size; i++)
@@ -35,15 +35,52 @@ void printSubArraySumBruteForce(int *arr, int size)
     }
 }
 
+
+// Brute force 
+void printBiggestSumSubArray(int *arr, int size)
+{
+    int subArrayStartingIdx = 0;
+    int subArrayEndingIdx = size - 1;
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i; j < size; j++)
+        {
+            int currentSubArraySum = 0;
+            for (int k = i; k <= j; k++)
+            {
+                currentSubArraySum += arr[k];
+            }
+            cout << "currentSubArraySum -> " << currentSubArraySum<< endl;
+            if (currentSubArraySum > sum)
+            {
+                subArrayStartingIdx = i;
+                subArrayEndingIdx = j;
+                sum = currentSubArraySum;
+            }
+        }
+    }
+
+    cout << "(";
+    while (subArrayStartingIdx <= subArrayEndingIdx)
+    {
+        cout << arr[subArrayStartingIdx] << ",";
+        subArrayStartingIdx++;
+    }
+    cout << ")";
+}
+
 int main()
 {
-    int arr[] = {10, 20, 30, 40, 50, 60};
+    int arr[] = {-40, 20, 0, 10, 80, 60};
     int size = sizeof(arr) / sizeof(int);
 
-    //printAllSubArray(arr, size); // Time complexity is O(n^3)
-    // There are toal of n^2 subarray of n size array
+    // printAllSubArray(arr, size); // Time complexity is O(n^3)
+    //  There are toal of n^2 subarray of n size array
 
-    printSubArraySumBruteForce(arr, size);
+    // printSubArraySumBruteForce(arr, size);
+
+    printBiggestSumSubArray(arr, size); // With brute force
 
     return 0;
 }
