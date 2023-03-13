db7c11176ee56dcdb822a55ec50d1e8963624399
Sagar Choudhary <sagarchaudhury@playsimple.in>
2023-03-13 12:27:08 +0530

    Pushing inbuilt sorting used in c++

---
+++ Basic/Sorting/sortInBuilt.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {5, 4, 1, 2, 3, -60};
+    sort(arr.begin(), arr.end());
+    for(int x: arr) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    int ar[] = {5, 4, 1, 2, 3, -60};
+    int size = sizeof(ar) / sizeof(int);
+    sort(ar, ar + size);
+
+    for (int x : ar)
+    {
+        cout << x << " ";
+    }
+
+    cout<< endl;
+    return 0;
+}
