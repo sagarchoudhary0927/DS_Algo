4f05988524c5d6ef3d25a9bc79a443745fccb88d
Sagar Choudhary <sagarchaudhury@playsimple.in>
2023-03-13 12:30:40 +0530

    Pushing comparator function and its use in compare function

--- Basic/Sorting/sortInBuilt.cpp
+++ Basic/Sorting/sortInBuilt.cpp
@@ -3,19 +3,24 @@
 #include <algorithm>
 using namespace std;
 
+bool compare(int a, int b ) {
+    // return a > b; // Decreasing order
+    return a < b; // Increasing order
+}
+
 int main()
 {
-    vector<int> arr = {5, 4, 1, 2, 3, -60};
-    sort(arr.begin(), arr.end());
-    for(int x: arr) {
-        cout << x << " ";
-    }
-    cout << endl;
+    // vector<int> arr = {5, 4, 1, 2, 3, -60};
+    // sort(arr.begin(), arr.end());
+    // for(int x: arr) {
+    //     cout << x << " ";
+    // }
+    // cout << endl;
 
     int ar[] = {5, 4, 1, 2, 3, -60};
     int size = sizeof(ar) / sizeof(int);
-    sort(ar, ar + size);
-    reverse(ar, ar + size);
+    sort(ar, ar + size, compare);
+    // reverse(ar, ar + size);
     for (int x : ar)
     {
         cout << x << " ";
